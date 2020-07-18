774754563ed84753362078fda5dc30b3a269021c
Jan <jlchoike@gmail.com>
2020-07-18 14:43:06 -0400

    Implemented remaining grid neighbor selection tests

--- groovy/src/test/groovy/gol/groovy/GridTest.groovy
+++ groovy/src/test/groovy/gol/groovy/GridTest.groovy
@@ -95,40 +95,142 @@ class GridTest extends Specification {
     }
 
     def "Grid gets neighbors of bottom left corner cell, in row-wise order"() {
-        //      0   1
-        //  0   N   N
-        //  1   C   N
+        //      0   1   2
+        //  0   X   X   X
+        //  1   N   N   X
+        //  2   C   N   X
+        def rowCount = 3
+        def columnCount = 3
+        def seed = new Random()
+        seed.setSeed(0)
+        def grid = Grid.newInstance(rowCount, columnCount, seed)
+
+        when:
+        List<Cell> neighbors = grid.getNeighborCells(2, 0)
+
+        then:
+        // neighbors = {(0,1), (1,0), (1,1)}
+        assert neighbors.size() == 3
+        assert neighbors[0].getRow() == 1 && neighbors[0].getColumn() == 0
+        assert neighbors[1].getRow() == 1 && neighbors[1].getColumn() == 1 
+        assert neighbors[2].getRow() == 2 && neighbors[2].getColumn() == 1  
     }
 
     def "Grid gets neighbors of bottom right corner cell, in row-wise order"() {
-        //      0   1
-        //  0   N   N
-        //  1   N   C        
+        //      0   1   2
+        //  0   X   X   X
+        //  1   X   N   N
+        //  2   X   N   C  
+        def rowCount = 3
+        def columnCount = 3
+        def seed = new Random()
+        seed.setSeed(0)
+        def grid = Grid.newInstance(rowCount, columnCount, seed)
+
+        when:
+        List<Cell> neighbors = grid.getNeighborCells(2, 2)
+
+        then:
+        // neighbors = {(0,1), (1,0), (1,1)}
+        assert neighbors.size() == 3
+        assert neighbors[0].getRow() == 1 && neighbors[0].getColumn() == 1
+        assert neighbors[1].getRow() == 1 && neighbors[1].getColumn() == 2 
+        assert neighbors[2].getRow() == 2 && neighbors[2].getColumn() == 1                
     }
 
     def "Grid gets neighbors of top middle cell, in row-wise order"() {
         //      0   1   2
         //  0   N   C   N
         //  1   N   N   N
+        //  2   X   X   X
+        def rowCount = 3
+        def columnCount = 3
+        def seed = new Random()
+        seed.setSeed(0)
+        def grid = Grid.newInstance(rowCount, columnCount, seed)
+
+        when:
+        List<Cell> neighbors = grid.getNeighborCells(0, 1)
+
+        then:
+        // neighbors = {(0,1), (1,0), (1,1)}
+        assert neighbors.size() == 5
+        assert neighbors[0].getRow() == 0 && neighbors[0].getColumn() == 0
+        assert neighbors[1].getRow() == 0 && neighbors[1].getColumn() == 2 
+        assert neighbors[2].getRow() == 1 && neighbors[2].getColumn() == 0
+        assert neighbors[3].getRow() == 1 && neighbors[3].getColumn() == 1
+        assert neighbors[4].getRow() == 1 && neighbors[4].getColumn() == 2                        
     }
     
     def "Grid gets neighbors of bottom middle cell, in row-wise order"() {
         //      0   1   2
-        //  0   N   N   N
-        //  1   N   C   N
+        //  0   X   X   X
+        //  1   N   N   N
+        //  2   N   C   N
+        def rowCount = 3
+        def columnCount = 3
+        def seed = new Random()
+        seed.setSeed(0)
+        def grid = Grid.newInstance(rowCount, columnCount, seed)
+
+        when:
+        List<Cell> neighbors = grid.getNeighborCells(2, 1)
+
+        then:
+        // neighbors = {(0,1), (1,0), (1,1)}
+        assert neighbors.size() == 5
+        assert neighbors[0].getRow() == 1 && neighbors[0].getColumn() == 0
+        assert neighbors[1].getRow() == 1 && neighbors[1].getColumn() == 1 
+        assert neighbors[2].getRow() == 1 && neighbors[2].getColumn() == 2 
+        assert neighbors[3].getRow() == 2 && neighbors[3].getColumn() == 0
+        assert neighbors[4].getRow() == 2 && neighbors[4].getColumn() == 2                        
     }
 
     def "Grid gets neighbors of left middle cell, in row-wise order"() {
-        //      0   1
-        //  0   N   N
-        //  1   C   N
-        //  2   N   N
+        //      0   1   2
+        //  0   N   N   X
+        //  1   C   N   X
+        //  2   N   N   X
+        def rowCount = 3
+        def columnCount = 3
+        def seed = new Random()
+        seed.setSeed(0)
+        def grid = Grid.newInstance(rowCount, columnCount, seed)
+
+        when:
+        List<Cell> neighbors = grid.getNeighborCells(1,0)
+
+        then:
+        // neighbors = {(0,1), (1,0), (1,1)}
+        assert neighbors.size() == 5
+        assert neighbors[0].getRow() == 0 && neighbors[0].getColumn() == 0
+        assert neighbors[1].getRow() == 0 && neighbors[1].getColumn() == 1 
+        assert neighbors[2].getRow() == 1 && neighbors[2].getColumn() == 1 
+        assert neighbors[3].getRow() == 2 && neighbors[3].getColumn() == 0
+        assert neighbors[4].getRow() == 2 && neighbors[4].getColumn() == 1                       
     }
 
     def "Grid gets neighbors of right middle cell, in row-wise order"() {
-        //      0   1
-        //  0   N   N
-        //  1   N   C
-        //  2   N   N
+        //      0   1   2
+        //  0   X   N   N
+        //  1   X   N   C
+        //  2   X   N   N
+        def rowCount = 3
+        def columnCount = 3
+        def seed = new Random()
+        seed.setSeed(0)
+        def grid = Grid.newInstance(rowCount, columnCount, seed)
+
+        when:
+        List<Cell> neighbors = grid.getNeighborCells(1,2)
+
+        then:
+        // neighbors = {(0,1), (1,0), (1,1)}
+        assert neighbors.size() == 5
+        assert neighbors[0].getRow() == 0 && neighbors[0].getColumn() == 1 
+        assert neighbors[1].getRow() == 0 && neighbors[1].getColumn() == 2 
+        assert neighbors[2].getRow() == 1 && neighbors[2].getColumn() == 1 
+        assert neighbors[3].getRow() == 2 && neighbors[3].getColumn() == 1
+        assert neighbors[4].getRow() == 2 && neighbors[4].getColumn() == 2                       
     }
 }
